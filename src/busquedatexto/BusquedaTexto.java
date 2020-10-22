30240927c32ed2769739be72ed46f5edbe28c26b
SamuelR <s_ramirezg_0624@outlook.com>
2020-10-22 12:41:18 -0500

    Primeros archivos programados

---
+++ src/busquedatexto/BusquedaTexto.java
@@ -0,0 +1,19 @@
+
+package busquedatexto;
+
+public class BusquedaTexto 
+{
+    public static void main(String[] args) 
+    {
+        GestionarArchivos g = new GestionarArchivos();
+        g.inicio();
+        
+        /*StringBuilder texto3 = new StringBuilder("Otra prueba");
+        texto3.append(" mas");
+        System.out.println( "Texto 3 es: " + texto3 );
+        texto3.insert(2, "1");
+        System.out.println( "Y ahora es: " + texto3 );
+        texto3.reverse();
+        System.out.println( "Y ahora: " + texto3 );*/
+    }
+}
---
+++ src/busquedatexto/GestionarArchivos.java
@@ -0,0 +1,158 @@
+
+package busquedatexto;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionarArchivos 
+{
+    private ArrayList <File> lista;
+    
+    public GestionarArchivos()
+    {
+        // constructor
+
+        lista = new ArrayList <File>();
+    }
+    
+    public void inicio()
+    {
+        File f = new File("C:\\www\\ito-jobs.localhost\\");
+        arbol(f);
+        
+        ArrayList <String> listaDirs = new ArrayList<>();
+        int cont = 0;
+        for(int i = 0; i < lista.size(); i++)
+        {
+            String ruta = lista.get(i).getAbsolutePath();
+            if(!ruta.contains(".git"))
+            {
+                File[] archivosCarpeta = lista.get(i).listFiles();
+                
+                for(int j = 0; j < archivosCarpeta.length; j++)
+                {
+                    if(archivosCarpeta[j].getAbsolutePath().contains(".php"))
+                    {
+                        //System.out.println(archivosCarpeta[j].getAbsolutePath());
+                        cont++;
+                        
+                        boolean flag = leerArchivo(archivosCarpeta[j], "Whoops");
+                        if(flag)
+                        {
+                            listaDirs.add(archivosCarpeta[j].getAbsolutePath());
+                        }
+                    }
+                }
+            }
+        }
+        
+        System.out.println("");
+        if(listaDirs.isEmpty())
+        {
+            System.out.println("No se encontro la cadena deseada");
+        }
+        else
+        {
+            for(String ss : listaDirs)
+            {
+                System.out.println("Encontrado en: " +ss);
+            }
+        }
+        
+        System.out.println("Total de archivos .php: " +cont);
+    }
+    
+    private boolean leerArchivo(File archivo, String busqueda)
+    {
+        //File archivo = new File("C:\\www\\ito-jobs.localhost\\arbol.doc");
+        System.out.println("Leyendo archivo " +archivo.getAbsolutePath());
+        
+        String cadena;
+        ArrayList <String> lineas = new ArrayList <String>();
+        try 
+        {
+            FileReader lector = new FileReader(archivo);
+            BufferedReader lectura = new BufferedReader(lector);          
+          
+            cadena = lectura.readLine(); 
+            
+            while(cadena != null)
+            {
+                cadena = lectura.readLine();
+                
+                if(cadena != null)
+                {
+                    lineas.add(cadena);
+                }
+            }   
+        } 
+        catch (FileNotFoundException ex) 
+        {
+           System.err.println("Error: " +ex);    
+        }
+        catch (IOException ex) 
+        {
+            System.err.println("Error: " +ex);    
+        }
+        
+        boolean bandera = false;
+        for(String l : lineas)
+        {   
+            if(l.contains(busqueda))
+            {
+                bandera = true;
+            }
+        }
+        return bandera;
+    }
+    
+    private void arbol(File file)
+    {
+        //System.out.println("Arbol: " +file.getAbsolutePath());
+        for(File f : file.listFiles())
+        {
+            if(f.isDirectory())
+            {
+                lista.add(new File(f.getAbsolutePath()));
+                
+                arbol(f); // usamos recursividad
+            }
+        }
+    }
+    
+    private String[] getFiles(String dir_path) 
+    {
+        String[] arr_res = null;
+        
+        File f = new File( dir_path );
+
+        if (f.isDirectory()) 
+        {
+            List<String> res = new ArrayList<>();
+            File[] arr_content = f.listFiles();
+
+            int size = arr_content.length; //System.out.println("Size: " +size);
+            
+            for(int i = 0; i < size; i++)
+            {
+
+                if (arr_content[i].isFile())
+                {
+                    if(arr_content[i].toString().contains(".php"))
+                        res.add(arr_content[i].toString());
+                }
+            }
+
+            arr_res = res.toArray(new String[0]);
+        } 
+        else
+            System.err.println("¡ Path NO válido !");
+
+        return arr_res;
+    }
+}
